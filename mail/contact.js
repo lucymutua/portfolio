7fef8a9df18128242238aa989fc5880de54bbc8d
lucymutua <laylataylor28@gmail.com>
2023-10-03 10:53:37 +0200

    emailjs and dependancies

--- mail/contact.js
+++ mail/contact.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
         },
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             emailjs.init("qNMB1E7aBXd1beX1T");
-            emailjs.send("service_s1iq7fr", "template_8bf9guo", {
+            emailjs.send("service_x1kf5r3", "template_8bf9guo", {
                 name: name,
                 email: email,
                 subject: subject,
@@ -57,8 +57,51 @@ $(function () {
         e.preventDefault();
         $(this).tab("show");
     });
-});
+}); 
 
-$('#name').focus(function () {
+ /*$('#name').focus(function () {
     $('#success').html('');
 });
+  $(document).ready(function () {
+    $("#contactForm").submit(function (e) {
+        e.preventDefault();
+
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var subject = $("#subject").val();
+        var message = $("#message").val();
+
+        // Validación de campos
+        if (!name || !email || !subject || !message) {
+            $('#success').html("<div class='alert alert-danger'>Please fill out all fields.</div>");
+            return;
+        }
+
+        // Enviar el formulario usando emailjs
+        emailjs.init("qNMB1E7aBXd1beX1T");
+
+        var templateParams = {
+            name: name,
+            email: email,
+            subject: subject,
+            message: message
+        };
+
+        emailjs.send("service_s1iq7fr", "template_8bf9guo", templateParams)
+            .then(function (response) {
+                console.log("Correo enviado con éxito:", response);
+                $('#success').html("<div class='alert alert-success'>Your message has been sent.</div>");
+                $('#contactForm')[0].reset();
+            })
+            .catch(function (error) {
+                console.error("Error al enviar el correo:", error);
+                $('#success').html("<div class='alert alert-danger'>Sorry, there was an error. Please try again later.</div>");
+            });
+    });
+
+    $('#name').focus(function () {
+        $('#success').html('');
+    });
+});
+
+});*/
--- mail/contact.php
+++ mail/contact.php
@@ -12,8 +12,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "lucia87mutua@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$header = "From: $email" . "\r\n";
+$header .= "Reply-To: $email";
+
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
